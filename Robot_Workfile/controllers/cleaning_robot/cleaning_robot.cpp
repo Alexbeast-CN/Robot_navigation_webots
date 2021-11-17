c84cf12659d5a9ed6e96a23048eee298144fead9
CN_Yifan_Wang <78745242+YifanWangCN@users.noreply.github.com>
2021-11-17 09:53:26 +0000

    add content

---
+++ Robot_Workfile/controllers/cleaning_robot/cleaning_robot.cpp
@@ -0,0 +1,44 @@
+// File:          cleaning_robot.cpp
+#include <webots/Robot.hpp>
+#include <stdio.h>     
+#include <math.h>      
+#include <webots/Led.hpp>
+#include <webots/Motor.hpp>
+#include <webots/Distance_sensor.h> 
+#include <webots/Position_sensor.h>
+
+// All the webots classes are defined in the "webots" namespace
+using namespace webots;
+
+
+int main(int argc, char **argv) {
+  // create the Robot instance.
+  Robot *robot = new Robot();
+
+  // get the time step of the current world.
+  int timeStep = (int)robot->getBasicTimeStep();
+
+  // You should insert a getDevice-like function in order to get the
+  // instance of a device of the robot. Something like:
+  //  Motor *motor = robot->getMotor("motorname");
+  //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
+  //  ds->enable(timeStep);
+
+  // Main loop:
+  // - perform simulation steps until Webots is stopping the controller
+  while (robot->step(timeStep) != -1) {
+    // Read the sensors:
+    // Enter here functions to read sensor data, like:
+    //  double val = ds->getValue();
+
+    // Process sensor data here.
+
+    // Enter here functions to send actuator commands, like:
+    //  motor->setPosition(10.0);
+  };
+
+  // Enter here exit cleanup code.
+
+  delete robot;
+  return 0;
+}
