f0c2a6dea39702a99b4f9cb0a584522590cff1e0
Tim.Chendaoming <1076079078@qq.com>
2021-11-18 17:39:25 +0000

    Map update

--- Robot_Workfolder/controllers/Initial/lib/Map.hpp
+++ Robot_Workfolder/controllers/Initial/lib/Map.hpp
@@ -1,5 +1,26 @@
 #pragma once
 
+<<<<<<< HEAD
+class Map
+{
+private:
+    /* data */
+public:
+    Map(/* args */);
+    ~Map();
+};
+
+Map::Map(/* args */)
+{
+}
+
+Map::~Map()
+{
+}
+
+
+
+=======
 #include <iostream>
 
 class Map
@@ -17,3 +38,4 @@ public:
 private:
 
 };
+>>>>>>> 483e69f71dde074142f04962a4ed2b64379417cb
