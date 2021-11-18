66b9acf6b95ff5471dbfc03be6d0b5e9618a2dcf
YifanWangCN <1244143158@qq.com>
2021-11-19 04:35:37 +0800

    modify Odometry API

--- Robot_Workfolder/controllers/Initial/lib/Odometry.cpp
+++ Robot_Workfolder/controllers/Initial/lib/Odometry.cpp
@@ -16,23 +16,35 @@
 // Environment variables
 using namespace webots;
 
+SweepRobot *SB;
 class Odometry
 {
+private:
+    float l_last_angle;
+    float r_last_angle;
+    float l_delta_angle;   
+    float r_delta_angle; 
+    float x;             
+    float y;            
+    float th;
+    float Current_left;
+    float Current_right;
 public:
+    Odometry(/* args */);
     Odometry(SweepRobot *I_Love_You_Last_Time)
     {
-        float l_last_angle = 0.00;
-        float r_last_angle = 0.00;
-        float l_delta_angle = 0;   
-        float r_delta_angle = 0; 
-        float x = 0;             
-        float y = 0;            
-        float th = 0; 
+        l_last_angle = 0.00;
+        r_last_angle = 0.00;
+        l_delta_angle = 0;   
+        r_delta_angle = 0; 
+        x = 0;             
+        y = 0;            
+        th = 0; 
     }
-    inline vector<float> Cordinates()
+    inline std::vector<float> Cordinates()
     {  
-        float Current_left = leftposition();
-        float Current_right = rightposition();
+        float Current_left = SB->leftposition();
+        float Current_right = SB->rightposition();
 
         l_delta_angle = Current_left - l_last_angle;
         r_delta_angle = Current_right - r_last_angle;
@@ -51,16 +63,15 @@ public:
         {
         th = th - (2*M_PI);
         }
-        return {x, y, th};
+        return {x,y,th};
     }
-private:
-    float l_last_angle;
-    float r_last_angle;
-    float l_delta_angle;   
-    float r_delta_angle; 
-    float x;             
-    float y;            
-    float th;
-    float Current_left;
-    float Current_right;
+    ~Odometry();
+};
+
+Odometry::Odometry(/* args */)
+{
+}
+
+Odometry::~Odometry()
+{
 }
