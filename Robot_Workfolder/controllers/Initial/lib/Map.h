6257e162e2bc24e22258437547a728fde4f824b9
Alexbeast-CN <1076079078@qq.com>
2021-11-18 23:33:23 +0000

    Add matrix class

---
+++ Robot_Workfolder/controllers/Initial/lib/Map.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Matrix.h"
+
+class Map
+{
+private:
+    /* data */
+public:
+    Map(/* args */);
+    Matrix Easy_Map(int, int);
+    ~Map();
+};
+
+Map::Map(/* args */)
+{
+}
+
+Map::~Map()
+{
+}
+
+
