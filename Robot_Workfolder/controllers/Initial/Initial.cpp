02ccc9a287bdcbf5a0949a4928bad05d3f0eba91
Tim.Chendaoming <1076079078@qq.com>
2021-11-22 17:59:27 +0000

    Can't solve

--- Robot_Workfolder/controllers/Initial/Initial.cpp
+++ Robot_Workfolder/controllers/Initial/Initial.cpp
@@ -49,50 +49,71 @@ int main(int argc, char **argv)
   float cor_x;
   float cor_y;
   float cor_theta;
+  float t;
   //create the Robot instance.
   Robot *robot = new Robot();
   SweepBot = new SweepRobot(robot);
   // Set a speed for robot
   double Regular_speed;
   Regular_speed = 40;
-  SweepBot->forward(Regular_speed);
-  SweepBot->delay_ms(1000);
+  int map_x;
+  int map_y;
+  int ahead_x;
+  int ahead_y;
+  int x_left;
+  int x_right;
+  int y_left;
+  int y_right;
 
-  SweepBot->turn_around_right(Regular_speed);
-  left_pos = SweepBot->leftposition();
-  right_pos = SweepBot->rightposition();
-  std::tie(cor_x,cor_y,cor_theta) = Odo.Cordinates(left_pos, right_pos);
-  SweepBot->stop();
-
-  /*
   // Main loop:
   while (robot->step(TIME_STEP) != -1)
   {
+    t = robot->getTime();
+    cout<<"Time is: " << t << endl;
     left_pos = SweepBot->leftposition();
     right_pos = SweepBot->rightposition();
     // std::cout<<"左轮的读数:"<<left_pos<<"  右轮的读数:"<<right_pos<<std::endl;
     std::tie(cor_x,cor_y,cor_theta) = Odo.Cordinates(left_pos, right_pos);
     
     // The inital place of the robot on the map
-    int map_x = cor_x*10 + 1;
-    int map_y = cor_y*10 + 1;
+    map_x = cor_x*10 + 1;
+    map_y = cor_y*10 + 1;
 
     // The coordinate that is 2 cells ahead of the robot
-    int ahead_x = map_x + 3*cos(abs(cor_theta));
-    int ahead_y = map_y + 3*sin(abs(cor_theta));
+    ahead_x = map_x + 2*cos(abs(cor_theta));
+    ahead_y = map_y + 2*sin(abs(cor_theta));
+    x_left = map_x - 1*sin(abs(cor_theta));
+    x_right = map_x + 1*sin(abs(cor_theta));
+    y_left = map_y - 1*cos(abs(cor_theta));
+    y_right = map_y + 1*cos(abs(cor_theta));
     cout << "Ahead x is " << ahead_x << ", ahead y is " << ahead_y << endl;
+
     // // Motion logic
-    // if (mat.Point(ahead_x,ahead_y)==1&&cor_theta<1)
-    //   SweepBot->turn_around_right(Regular_speed);
-    // else if (mat.Point(ahead_x,ahead_y)==1&&cor_theta>3)
-    //   SweepBot->turn_around_left(Regular_speed);
-    // else 
-    //   SweepBot->forward(Regular_speed);
+    if (mat.Point(ahead_x,ahead_y)==1)
+    {
+      if (mat.Point(map_x,y_right)==1)
+      {
+        if (mat.Point(map_x,y_left)==1)
+          SweepBot->stop();
+        else
+          SweepBot->turn_around_left(Regular_speed);
+      }
+      else if (mat.Point(map_x,y_left)==1)
+      {
+        if (mat.Point(map_x,y_right)==1)
+          SweepBot->stop();
+        else
+          SweepBot->turn_around_right(Regular_speed);
+      }
+      else 
+        SweepBot->turn_around_right(Regular_speed);
+    }
+    else
+      SweepBot->forward(Regular_speed);
+    
+    robot->step(200);
   }
-    */
-
   // Enter exit cleanup code here.
-
   delete robot;
   delete SweepBot;
   return 0;
--- Robot_Workfolder/controllers/Initial/lib/Odometry.h
+++ Robot_Workfolder/controllers/Initial/lib/Odometry.h
@@ -49,7 +49,7 @@ public:
         r_last_angle = Current_right;
 
         forward_contribution = ((WHEEL_RADIUS * l_delta_angle)/2) + ((WHEEL_RADIUS * r_delta_angle)/2);
-        theta_contribution = (r_delta_angle-l_delta_angle)*(0.5*(WHEEL_RADIUS)/ROBOT_RADIUS);
+        theta_contribution = (l_delta_angle-r_delta_angle)*(0.5*(WHEEL_RADIUS)/ROBOT_RADIUS);
   
         x  = x + (forward_contribution * cos(th));
         y  = y + (forward_contribution * sin(th));
